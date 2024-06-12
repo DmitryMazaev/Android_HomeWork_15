4c41b2c131bf332c254b3d1f20c7f43e05fda35f
Dmitry <dmitry91ivanov@yandex.ru>
2024-06-12 19:18:27 +0300

    456

--- app/src/main/java/com/example/android_homework_15/FirstFragment.kt
+++ app/src/main/java/com/example/android_homework_15/FirstFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.android_homework_15.databinding.FragmentFirstBinding
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -16,7 +20,14 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels {
+        object: ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val wordsDao: WordsDao = (requireActivity().application as App).db.wordsDao()
+                return MainViewModel(wordsDao) as T
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,10 +38,18 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonAdd.setOnClickListener {
+        //(requireActivity().application as App).db.wordsDao()
+        viewLifecycleOwner.lifecycleScope
+            .launch {
+                binding.buttonAdd.setOnClickListener {
+                    viewModel.addWord(binding.editText.text.toString(), 0)
+                    binding.words.text = viewModel.allWords.toString()
+                }
+        }.start()
+        /*binding.buttonAdd.setOnClickListener {
             viewModel.addWord(binding.editText.text.toString(), 0)
             binding.words.text = viewModel.allWords.toString()
-        }
+        }*/
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/android_homework_15/MainViewModel.kt
+++ app/src/main/java/com/example/android_homework_15/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.android_homework_15
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -10,7 +11,7 @@ class MainViewModel(private val wordsDao: WordsDao): ViewModel() {
     val allWords: Flow<List<Word>> = this.wordsDao.getAllWords()
 
     fun addWord(string: String, count: Int) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             wordsDao.insertWord(
                 NewWord(
                     oneWord = string,
